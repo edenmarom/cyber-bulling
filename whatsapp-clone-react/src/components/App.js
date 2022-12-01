c8e65097186a334faceb96a601c37e2381cf6e17
Eden Marom <EdenMarom@gmail.com>
2022-12-01 19:45:27 +0200

    query example

--- whatsapp-clone-react/src/components/App.js
+++ whatsapp-clone-react/src/components/App.js
@@ -5,6 +5,15 @@ import Chat from "../components/Chat";
 import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useStateValue } from "../StateProvider";
+import axios from "axios";
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
@@ -27,7 +36,6 @@ function App() {
     //         </Router>
     //       </div>
     //     )}
-
     // </div>
 
     <div className="app">
@@ -44,8 +52,31 @@ function App() {
               </Route>
             </Switch>
           </Router>
+          <QueryClientProvider client={queryClient}>
+            <Example />
+          </QueryClientProvider>
         </div>
       }
+    </div>
+  );
+}
+
+function Example() {
+  const { isLoading, error, data, isFetching } = useQuery(["repoData"], () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.data)
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      <h1>{"query example"}</h1>
+      <p>{"id: " + data[9].id}</p>
+      <p>{data[9].title}</p>
     </div>
   );
 }
