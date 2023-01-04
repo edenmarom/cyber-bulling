739cc79aac13e262aa240855f3df4ccfa81d67e8
Peleg Admi <pelegadmi@gmail.com>
2023-01-04 20:39:51 +0200

    backend linkage & firebase remove

--- whatsapp-clone-react/src/components/App.js
+++ whatsapp-clone-react/src/components/App.js
@@ -1,84 +1,86 @@
 import "../css/App.css";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Sidebar from "../components/Sidebar";
 import Chat from "../components/Chat";
 import Login from "./Login";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useStateValue } from "../StateProvider";
-import axios from "axios";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {useStateValue} from "../StateProvider";
+import http from "../utils/http-communication.ts";
 import {
-  useQuery,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
+    useQuery,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
 } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [{ user }, dispatch] = useStateValue();
-  return (
-    // <div className="app">
-    //     {!user ? (
-    //       <Login/>
-    //     ):(
-    //       <div className="app_body">
-    //         <Router>
-    //           <Sidebar/>
-    //           <Switch>
-    //             <Route path="/rooms/:roomId">
-    //               <Chat/>
-    //             </Route>
-    //             <Route path="/">
-    //               <Chat/>
-    //             </Route>
-    //           </Switch>
-    //         </Router>
-    //       </div>
-    //     )}
-    // </div>
+    const [{user}, dispatch] = useStateValue();
+    return (
+        // <div className="app">
+        //     {!user ? (
+        //       <Login/>
+        //     ):(
+        //       <div className="app_body">
+        //         <Router>
+        //           <Sidebar/>
+        //           <Switch>
+        //             <Route path="/rooms/:roomId">
+        //               <Chat/>
+        //             </Route>
+        //             <Route path="/">
+        //               <Chat/>
+        //             </Route>
+        //           </Switch>
+        //         </Router>
+        //       </div>
+        //     )}
+        // </div>
 
-    <div className="app">
-      {
-        <div className="app_body">
-          <Router>
-            <Sidebar />
-            <Switch>
-              <Route path="/rooms/:roomId">
-                <Chat />
-              </Route>
-              <Route path="/">
-                <Chat />
-              </Route>
-            </Switch>
-          </Router>
-          <QueryClientProvider client={queryClient}>
-            <Example />
-          </QueryClientProvider>
+        <div className="app">
+            {
+                <div className="app_body">
+                    <Router>
+                        <Sidebar/>
+                        <Switch>
+                            <Route path="/rooms/:roomId">
+                                <Chat/>
+                            </Route>
+                            <Route path="/">
+                                <Chat/>
+                            </Route>
+                        </Switch>
+                    </Router>
+                    <QueryClientProvider client={queryClient}>
+                        <Example/>
+                    </QueryClientProvider>
+                </div>
+            }
         </div>
-      }
-    </div>
-  );
+    );
 }
 
 function Example() {
-  const { isLoading, error, data, isFetching } = useQuery(["repoData"], () =>
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.data)
-  );
+    const {isLoading, error, data, isFetching} = useQuery(["repoData"], () =>
+            http
+                .get("/users")
+                .then((res) =>
+                    res.data
+                )
+        );
+    console.log(data);
 
-  if (isLoading) return "Loading...";
+    if (isLoading) return "Loading...";
 
-  if (error) return "An error has occurred: " + error.message;
+    if (error) return "An error has occurred: " + error.message;
 
-  return (
-    <div>
-      <h1>{"query example"}</h1>
-      <p>{"id: " + data[9].id}</p>
-      <p>{data[9].title}</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{"query example"}</h1>
+            <p>{data.data[0].nickname}</p>
+        </div>
+    );
 }
 
 export default App;
--- whatsapp-clone-react/src/components/Sidebar.js
+++ whatsapp-clone-react/src/components/Sidebar.js
@@ -6,7 +6,7 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
-import db from "./firebase";
+// import db from "./firebase";
 import { useStateValue } from "../StateProvider";
 
 function Sidebar(props) {
