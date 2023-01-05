152155f407bb20893cd0f1b6ecdb2b379231c410
Peleg Admi <pelegadmi@gmail.com>
2023-01-05 19:34:30 +0200

    fixed link bug

--- whatsapp-clone-react/src/components/App.js
+++ whatsapp-clone-react/src/components/App.js
@@ -1,10 +1,8 @@
 import "../css/App.css";
-import React, {useState} from "react";
+import React from "react";
 import Sidebar from "../components/Sidebar";
 import Chat from "../components/Chat";
-import Login from "./Login";
-import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
-import {useStateValue} from "../StateProvider";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import http from "../utils/http-communication.ts";
 import {
     useQuery,
@@ -19,77 +17,24 @@ import ChatPreview from "./ChatPreview";
 const queryClient = new QueryClient();
 
 function App() {
-    const [{user}, dispatch] = useStateValue();
     return (
-        // <div className="app">
-        //     {!user ? (
-        //       <Login/>
-        //     ):(
-        //       <div className="app_body">
-        //         <Router>
-        //           <Sidebar/>
-        //           <Switch>
-        //             <Route path="/rooms/:roomId">
-        //               <Chat/>
-        //             </Route>
-        //             <Route path="/">
-        //               <Chat/>
-        //             </Route>
-        //           </Switch>
-        //         </Router>
-        //       </div>
-        //     )}
-        // </div>
-        <div className="app">{
-            <div className="app_body">
-
-                <Router>
-                    <Sidebar/>
-                    <Routes>
+        <div className="app">
+            <BrowserRouter>
+                <Sidebar/>
+                <Routes>
+                    <Route path="/">
                         <Route index element={<SignIn/>}/>
                         <Route path="admin" element={<Admin/>}/>
                         <Route path="chatpreview" element={<ChatPreview/>}/>
-                        <Route path="/rooms/:roomId" element={<Chat/>}/>
-                        {/*<Route path="/">*/}
-                        {/*    <Chat/>*/}
-                        {/*</Route>*/}
-                    </Routes>
-                </Router>
-                <QueryClientProvider client={queryClient}>
-                    <Example/>
-                </QueryClientProvider>
+                        <Route path="rooms/:roomId" element={<Chat/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
 
-                {/*<BrowserRouter>*/}
-                {/*    <Sidebar />*/}
-                {/*    <Routes>*/}
-                {/*        <Route path="/">*/}
-                {/*            <Route index element={<SignIn/>}/>*/}
-                {/*            <Route path="admin" element={<Admin/>}/>*/}
-                {/*            <Route path="chatpreview" element={<ChatPreview/>}/>*/}
-                {/*            <Route path="rooms" element={<Chat/>}/>*/}
-                {/*        </Route>*/}
-                {/*    </Routes>*/}
-
-                {/*</BrowserRouter>*/}
-                {/*{*/}
-                {/*  <div className="app_body">*/}
-                {/*    <Router>*/}
-                {/*      <Sidebar />*/}
-                {/*      <Switch>*/}
-                {/*        <Route path="/rooms/:roomId">*/}
-                {/*          <Chat />*/}
-                {/*        </Route>*/}
-                {/*        <Route path="/">*/}
-                {/*          <Chat />*/}
-                {/*        </Route>*/}
-                {/*      </Switch>*/}
-                {/*    </Router>*/}
-                {/*    <QueryClientProvider client={queryClient}>*/}
-                {/*      <Example />*/}
-                {/*    </QueryClientProvider>*/}
-                {/*  </div>*/}
-                {/*}*/}
-            </div>}
+            {/*<QueryClientProvider client={queryClient}>*/}
+            {/*    <Example/>*/}
+            {/*</QueryClientProvider>*/}
+            {/*</div>*/}
         </div>
     );
 }
--- whatsapp-clone-react/src/components/Chat.js
+++ whatsapp-clone-react/src/components/Chat.js
@@ -1,10 +1,10 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Avatar, IconButton} from '@material-ui/core';
 import {AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons';
 import MicIcon from '@material-ui/icons/Mic';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import "../css/Chat.css";
-import { useParams } from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 // import db from './firebase';
 // import firebase from 'firebase';
 import {useStateValue} from "../StateProvider";
@@ -17,8 +17,8 @@ function Chat() {
     const [messages, setMessages] = useState([]);
     const [{user}, dispatch] = useStateValue();
 
-    useEffect(()=>{
-        if(roomId){
+    useEffect(() => {
+        if (roomId) {
             // db.collection('rooms').doc(roomId).onSnapshot(snapshot => {
             //     setRoomName(snapshot.data().name);
             // });
@@ -30,10 +30,10 @@ function Chat() {
             setRoomName("room1");
             setMessages([]);
         }
-    },[roomId])
+    }, [roomId])
 
     useEffect(() => {
-        setSeed(Math.floor(Math.random() * 5000));        
+        setSeed(Math.floor(Math.random() * 5000));
     }, [roomId]);
 
     const sendMessage = (e) => {
@@ -48,50 +48,53 @@ function Chat() {
     }
 
     return (
-        <div className='chat'>
-            <div className='chat_header'>
-                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
-                <div className='chat_headerInfo'>
-                    <h3 className='chat-room-name'>{roomName}</h3>
-                    <p className='chat-room-last-seen'>
-                        Last seen {" "}
-                        {new Date(
-                            messages[messages.length - 1]?.
-                            timestamp?.toDate()
-                        ).toUTCString()}
-                    </p>
+        <div className="app_body">
+            <div className='chat'>
+                <div className='chat_header'>
+                    <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                    <div className='chat_headerInfo'>
+                        <h3 className='chat-room-name'>{roomName}</h3>
+                        <p className='chat-room-last-seen'>
+                            Last seen {" "}
+                            {new Date(
+                                messages[messages.length - 1]?.timestamp?.toDate()
+                            ).toUTCString()}
+                        </p>
+                    </div>
+                    <div className="chat_headerRight">
+                        <IconButton>
+                            <SearchOutlined/>
+                        </IconButton>
+                        <IconButton>
+                            <AttachFile/>
+                        </IconButton>
+                        <IconButton>
+                            <MoreVert/>
+                        </IconButton>
+
+                    </div>
                 </div>
-                <div className="chat_headerRight">
-                    <IconButton>
-                        <SearchOutlined/>
-                    </IconButton>
-                    <IconButton>
-                        <AttachFile/>
-                    </IconButton>
-                    <IconButton>
-                        <MoreVert/>
-                    </IconButton>
-                    
+                <div className='chat_body'>
+                    {messages.map(message => (
+                        <p className={`chat_message ${message.name == user.displayName && 'chat_receiver'}`}>
+                            <span className="chat_name">{message.name}</span>
+                            {message.message}
+                            <span
+                                className="chat_timestemp">{new Date(message.timestamp?.toDate()).toUTCString()}</span>
+                        </p>
+                    ))}
                 </div>
+                <div className='chat_footer'>
+                    <InsertEmoticonIcon/>
+                    <form>
+                        <input value={input} onChange={(e) => setInput(e.target.value)} type="text"
+                               placeholder="Type a message"/>
+                        <button type="submit" onClick={sendMessage}> Send a Message</button>
+                    </form>
+                    <MicIcon/>
+                </div>
+
             </div>
-            <div className='chat_body'>
-                {messages.map(message => (
-                    <p className={`chat_message ${ message.name == user.displayName && 'chat_receiver'}`}>
-                        <span className="chat_name">{message.name}</span>
-                        {message.message}
-                        <span className="chat_timestemp">{new Date(message.timestamp?.toDate()).toUTCString()}</span>
-                    </p>
-                ))}
-            </div>
-            <div className='chat_footer'>
-                <InsertEmoticonIcon />
-                <form>
-                    <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Type a message"/>
-                    <button type="submit" onClick={sendMessage}> Send a Message</button>
-                </form>
-                <MicIcon/>
-            </div>
-            
         </div>
     )
 }
