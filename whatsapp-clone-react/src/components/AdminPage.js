aa0655e064d205d725f5a58e32594ba4a8a0b3c3
Eden Marom <EdenMarom@gmail.com>
2023-02-25 16:48:23 +0200

    deleting old folder

--- whatsapp-clone-react/src/components/AdminPage.js
+++
@@ -1,7 +0,0 @@
-import AdminSideBar from "./AdminSideBar";
-import {React} from "react";
-export default function AdminPage() {
-    return(<div>
-        <AdminSideBar></AdminSideBar>
-    </div>);
-}
